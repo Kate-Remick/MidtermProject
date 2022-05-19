c954b1560cb6c42d7488cb2e91b1848f66aaa90f
Katherine <remick.ka@gmail.com>
2022-05-19 13:20:01 -0700

    spring boot with mvc created

---
+++ Spotr/src/main/java/com/skilldistillery/fitnessfinder/FitnessFinderSpotrApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fitnessfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessFinderSpotrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessFinderSpotrApplication.class, args);
+	}
+
+}
---
+++ Spotr/src/test/java/com/skilldistillery/fitnessfinder/FitnessFinderSpotrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fitnessfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessFinderSpotrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
